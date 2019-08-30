4e98e842b34e6016482f03b336c51fe80511ab0c
Steve Holland <sdh4@iastate.edu>
2019-08-30 08:43:46 -0500

    Various estimation bugfixes/improvements. Extract closure_lowest_avg_load; show specimen ID's on plots.

--- demos/estparam.py
+++ demos/estparam.py
@@ -11,14 +11,13 @@ from matplotlib import pyplot as pl
 from theano.compile.ops import as_op
 
 from crackheat_surrogate.load_surrogate import nonnegative_denormalized_surrogate
-from crackheat_surrogate.training_eval import training_eval
 
 from vibro_estparam.estparam import estparam
 
 from function_as_script import scriptify
 
 if __name__=="__main__":
-    datapath='/tmp/data/'
+    datapath='/tmp/data2/'
     
     surrogateext = "_surrogate.json"
     crackheatfiles = glob.glob(os.path.join(datapath,"*_crackheat_table.csv"))
@@ -29,19 +28,20 @@ if __name__=="__main__":
     
     #context = cl.create_some_context()
     #accel_trisolve_devs=(os.getpid(),tuple([ (dev.platform.name,dev.name) for dev in context.devices]))
-    #accel_trisolve_devs=(os.getpid(), (('NVIDIA CUDA', 'Quadro GP100'),)) 
-    accel_trisolve_devs = (os.getpid(),(('Intel(R) OpenCL HD Graphics', 'Intel(R) Gen9 HD Graphics NEO'),))
+    accel_trisolve_devs=(os.getpid(), (('NVIDIA CUDA', 'Quadro GP100'),)) 
+    #accel_trisolve_devs = (os.getpid(),(('Intel(R) OpenCL HD Graphics', 'Intel(R) Gen9 HD Graphics NEO'),))
     #accel_trisolve_devs = None
 
 
     crack_specimens = [ os.path.split(filename)[1][:(-len(surrogateext))] for filename in surrogatefiles ]  # we number cracks according to their index in this list
 
+    #estimator = estparam.fromfilelists(crack_specimens,crackheatfiles,surrogatefiles,accel_trisolve_devs)
     estimator = scriptify(estparam.fromfilelists)(crack_specimens,crackheatfiles,surrogatefiles,accel_trisolve_devs)
 
-    estimator.load_data()
+    scriptify(estimator.load_data)(filter_outside_closure_domain=True)
     
     #estimator.posterior_estimation(1000,4,cores=4)
     scriptify(estimator.posterior_estimation)(1000,4,cores=4)
-
-    estimator.plot_and_estimate()
+    scriptify(estimator.plot_and_estimate)()
+    pl.show()
     pass
--- vibro_estparam/estparam.py
+++ vibro_estparam/estparam.py
@@ -7,11 +7,9 @@ import numpy as np
 import theano.tensor as tt
 import pymc3 as pm
 import pandas as pd
-from matplotlib import pyplot as pl
 from theano.compile.ops import as_op
 
 from crackheat_surrogate.load_surrogate import nonnegative_denormalized_surrogate
-from crackheat_surrogate.training_eval import training_eval
 
 
 
@@ -69,27 +67,38 @@ class estparam(object):
         return cls(crack_specimens=crack_specimens,
                    crackheatfiles=crackheatfiles,
                    surrogatefiles=surrogatefiles,
-                   accel_trisolve_devs=None)
+                   accel_trisolve_devs=accel_trisolve_devs)
 
-    def load_data(self):
+    def load_data(self,filter_outside_closure_domain=True):
         
         
         self.crack_surrogates = [ nonnegative_denormalized_surrogate.fromjsonfile(filename) for filename in self.surrogatefiles ]  
 
         self.crackheatfile_dataframes = [ pd.read_csv(crackheat_file) for crackheat_file in self.crackheatfiles ]
 
-        self.crackheat_table = pd.concat(self.crackheatfile_dataframes,ignore_index=True)
-    
-        # Drop rows in crackheat_table with NaN ThermalPower
-        NaNrownums = np.where(np.isnan(self.crackheat_table["ThermalPower (W)"].values))[0]
-    
-        self.crackheat_table = self.crackheat_table.drop(NaNrownums,axis=0)
-        
+        self.crackheat_table = pd.concat(self.crackheatfile_dataframes,ignore_index=True) # NOTE: Pandas warning about sorting along non-concatenation axis should go away once all data has timestamps
 
         # Add specimen numbers to crackheat table
         # If this next line is slow, can easily be accelerated with a dictionary!
         specimen_nums = np.array([ self.crack_specimens.index(specimen) for specimen in self.crackheat_table["Specimen"].values ],dtype=np.uint32)
         self.crackheat_table["specimen_nums"]=specimen_nums
+
+        # Look up closure_lowest_avg_load_used from values loaded into surrogate
+        closure_lowest_avg_load_used = np.array([ self.crack_surrogates[specimen_num].closure_lowest_avg_load_used for specimen_num in self.crackheat_table["specimen_nums"].values ])
+        self.crackheat_table["closure_lowest_avg_load_used"]=closure_lowest_avg_load_used
+        
+        # Drop rows in crackheat_table with NaN ThermalPower
+        NaNrownums = np.where(np.isnan(self.crackheat_table["ThermalPower (W)"].values))[0]
+    
+        self.crackheat_table = self.crackheat_table.drop(NaNrownums,axis=0)
+
+        if filter_outside_closure_domain:
+            # Drop rows in crackheat_table with bending stress less than closure_lowest_avg_load_used
+            outside_closure_domain_rownums = np.where((~np.isnan(self.crackheat_table["closure_lowest_avg_load_used"].values)) & (self.crackheat_table["closure_lowest_avg_load_used"].values > self.crackheat_table["BendingStress (Pa)"].values))[0]
+            self.crackheat_table = self.crackheat_table.drop(outside_closure_domain_rownums,axis=0)
+                                                       
+            pass
+            
         
         pass
     
@@ -131,7 +140,10 @@ class estparam(object):
             pass
         pass
 
-    def plot_and_estimate(self,mu_zone=(0.2,0.5),msqrtR_zone=(.37e8,.43e8),marginal_bins=50,joint_bins=(230,200)):
+    def plot_and_estimate(self,mu_zone=(0.05,0.5),msqrtR_zone=(.37e8,.43e8),marginal_bins=50,joint_bins=(230,200)):
+
+        from matplotlib import pyplot as pl
+        import cycler
 
         pl.figure()
         pm.traceplot(self.trace);
@@ -166,18 +178,37 @@ class estparam(object):
         self.msqrtR_estimate = (hist_msqrtR_edges[histpeakpos[1]]+hist_msqrtR_edges[histpeakpos[1]+1])/2.0
     
         # Compare
-        self.predicted = self.predict_crackheating._FromFunctionOp__fn(self.crackheat_table["specimen_nums"].values, self.mu_estimate,self.crackheat_table["BendingStress (Pa)"].values,self.crackheat_table["DynamicStressAmpl (Pa)"].values,self.msqrtR_estimate)*self.crackheat_table["ExcFreq (Hz)"].values
+        self.predicted = self.predict_crackheating(self.crackheat_table["specimen_nums"].values, self.mu_estimate,self.crackheat_table["BendingStress (Pa)"].values,self.crackheat_table["DynamicStressAmpl (Pa)"].values,self.msqrtR_estimate)*self.crackheat_table["ExcFreq (Hz)"].values
         
-        # with:
-        self.actual = self.crackheat_table["ThermalPower (W)"].values
+        # add to crackheat_table
         
+        self.crackheat_table["predicted"]=self.predicted
+
+        ## with:
+        #self.actual = self.crackheat_table["ThermalPower (W)"].values
+
+        # Group predicted and actual heating by specimen
+        specimen_grouping = self.crackheat_table.groupby("Specimen")
+
+        specimen_groups = [ specimen_grouping.get_group(specimen) for specimen in specimen_grouping.groups ]
+        predicted_by_specimen = [ specimen_group["predicted"].values for specimen_group in specimen_groups ]
+        actual_by_specimen = [ specimen_group["ThermalPower (W)"].values for specimen_group in specimen_groups ]
+        specimen_group_specimens = list(specimen_grouping.groups) 
+
+        markerstyle_cycler=cycler.cycler(marker=['o','v','^','<','>','s','p','+','x','D'])()
+        
+
         pl.figure()
-        pl.plot(self.predicted,self.actual,'x',
-                (0,np.max(self.predicted)),(0,np.max(self.predicted)),'-')
-        pl.xlabel('Predicted heating from model (W)')
-        pl.ylabel('Actual heating from experiment (W)')
+        pl.clf()
+        #pl.plot(self.predicted,self.actual,'x',
+        #        (0,np.max(self.predicted)),(0,np.max(self.predicted)),'-')
+        [ pl.plot(predicted_by_specimen[idx]*1e3,actual_by_specimen[idx]*1e3,linestyle='',**next(markerstyle_cycler)) for idx in range(len(specimen_group_specimens)) ] 
+        pl.plot((0,np.max(self.predicted)*1e3),(0,np.max(self.predicted)*1e3),'-')
+        pl.legend(specimen_group_specimens,loc='best')
+        pl.xlabel('Predicted heating from model (mW)')
+        pl.ylabel('Actual heating from experiment (mW)')
+        pl.title('mu_estimate=%g; msqrtR_estimate=%g' % (self.mu_estimate,self.msqrtR_estimate))
         pl.grid()
-        pl.show()
         
-        pass
+        return (self.mu_estimate,self.msqrtR_estimate)
     pass
