Fix missing excfreq_median calculation

vibro_estparam/estparam.py
@@ -630,8 +630,8 @@ class estparam(object):
             self.trace = pm.sample(steps_per_chain, step=self.step,chains=num_chains, cores=cores,tune=tune,discard_tuned_samples=True,random_seed=list(range(cores))) #tune=tune cores=cores chains=num_chains  ***!!! rememeber to unset random seed eventually!!!
             trace_df = pm.backends.tracetab.trace_to_dataframe(self.trace,include_transformed=True)
 
-
-        
+            excfreq_median = np.median(self.crackheat_table["ExcFreq (Hz)"].values)
+            
             return (trace_df,
                     # Scalars
                     self.mu_prior_mu,